85ba1632934b29abe0952066f7fd01634ad06fd6
rxsnk <iwayanikaaa@gmail.com>
2025-09-21 14:06:54 +0800

    Add files via upload

---
+++ rexxa_assignment.cpp
@@ -0,0 +1,21 @@
+
+using namespace std;
+#include <iostream>
+
+int main() {
+    cout << "                                                                                                         |||||||||||||||||||||||||||||||||\n";
+    cout << "                                                                                                        ||||||                       ||||||\n";
+    cout << "          {{{   {{{     {{{    {{{     {{{    {{{     {{{  {{{    {{{   {{{  {{{  {{{    {{{     {{{   MMMMMM      MMMMMMMMM AAAA NNNNNNNNN      NNNNN\n";
+    cout << "    <<<< ||||  ||||  ||||||   ||||   |||||   ||||    ||||  |||   ||||  ||||  |||  |||   ||||    ||||   MMMMMM      MMMMMMMMM AAAA NNNNNNNNN      NNNNN\n";
+    cout << "{{ ((| CCCCCC  HHHH  HHHHHH  AAAAAA  IIIIII  NNNN     NNN  SSSSSSSSSSS  AAAAAAAAAA WWW  WW   WWWWWWWW MMMMMMMM    MMMMMMMMM AAAAAA NNNNNNNNN     NNNNN||||||||||\n";
+    cout << "  ((( CCCCCCC  HHHH  HHHHH  AAAAAAAA  IIIII  NNNNN    NNN  SSS  SSSSSS  AAAAAAAAAA  WWW WWW  WWWWWWW  MMMMMMMM    MMMMMMMM AAAAAAAA NNNNNNNNN    NNNNN        ||\n";
+    cout << "  ||| CCC      HHHH  HHHH  AAAAAAAAAA IIIII  NNNNNN   NNN  SS  SSSSS    AAAAAAAAAAA  WWW WWW WWWWWW  MMMMMMMMM   MMMMMMMM AAAAAAAAA NNNNNNNNN    NNNNN        ||\n";
+    cout << "{ ||| CCC      HHHH  HHHH  AAAAA AAAA  IIII  NNNNNN   NNN  SSS          AAAAAA AAAA  WWWWWWWWWWWWWW  MMMMMMMMMM  MMMMMMM AAAAA  AAAAA NNNNNNNNN  NNNNN        ||\n";
+    cout << "  ||| CCC      HHHHHHHHH  AAAAA   AAAA IIII  NNNNNNN  NNN  SSSSSSSSSSS  AAAAA  AAAAA WWWWWWWWWWWWW  MMMMMMMMMM  MMMMMM AAAAAA   AAAAA NNNNNNNNNNNNNNNN        ||\n";
+    cout << "  ||| CCC      HHHH  HHH  AAAAAAAAAAAAA III  NBRYLENNNNNN  SSSSSSSSSSS  AAAAAAAAAAAA  WWWW  WWWWWW  MMMMMMMMMMMMMMMMM AAAAAA    AAAAAA NNNNNNNNNNNNNNN        ||\n";
+    cout << "{ ||| CCC      HHHH  HHH  AAAAAAAAAAAAA III  NNNN   NNNNN         SSSS  AAAAAAAAAAAAA WWWW   WWWWW MMMM   MMMMM MMMM AAAAAAA    AAAAAAA NNNNNN  NNNNNN        ||\n";
+    cout << "  ((( CCCCCCC  HHHH  HHH AAAAA    AAAAA III  NNNN   NNNNN  SSSSSSSSSSS  AAAAA  AAAAAA WWWW   WWWW  MMMM   MMM  MMMM AAAAAAAAAAAAAAAAAAAA NNNNN   NNNNN        ||\n";
+    cout << "{{ ((| CCCCCC  HHHH  HHH AAAAA    AAAAA III  NNNN    NNNN  SSSSSSSSSSS  JAYAA   AAAAAA WWW    WWW  MMM     M   MMM AAAAAAAAAAAAAAAAAAAAA NNNNN   NNNNN||||||||||\n";
+    cout << "    <<<< ||||  ||||  |||||   ||||     |||||  ||||    ||||  |||   ||||  ||||  |||   |||  ||||  |||| MMMM        MMM AAAAAAA       AAAAAAAA NNNN    NNNN\n";
+    cout << "          {{{   {{{    {{{    {{{       {{{   {{{     {{{  {{{    {{{   {{{  {{{   {{{   {{{  {{{ MMMMM        MM AAAAAAA         AAAAAAAA NNN    NNNN\n";
+}
